bf0e3a7087746c629432391d2a82d818e2af6ef7
Omkar Chandorkar <gotenksIN@aospa.co>
2025-01-10 13:18:23 +0900

    system: bt: Add aptX apps
    Change-Id: I856fee79e65ecdb9058c4dcf10a6117baef1825b
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/bt/bt-vendor.mk
+++ system/bt/bt-vendor.mk
@@ -5,6 +5,13 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/common/system/bt
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/common/system/bt/proprietary/system_ext/etc/permissions/privapp-permissions-aptxals.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-aptxals.xml \
+    vendor/qcom/common/system/bt/proprietary/system_ext/etc/permissions/privapp-permissions-aptxui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-aptxui.xml
 
 PRODUCT_PACKAGES += \
-    com.qualcomm.qti.ant@1.0
+    com.qualcomm.qti.ant@1.0 \
+    aptxacu \
+    aptxals \
+    aptxui \
+    aptxalsOverlayCreateProject
